3f2488d576cbe8ed245d308173b55c37babdab24
edson <edsonfilho734@gmail.com>
2022-03-30 23:45:41 -0300

    Finalizando o projeto

---
+++ servicediscovery/src/test/java/com/facisa/arquitetura/servicediscovery/ServicediscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facisa.arquitetura.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicediscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
